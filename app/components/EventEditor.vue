805f8f1af16afbb233a7a6b03eb647c80d78ecd4
tomon9086 <tomon9086@gmail.com>
2021-11-27 18:39:40 +0900

    apply grid system to event-editor

--- app/components/EventEditor.vue
+++ app/components/EventEditor.vue
@@ -1,52 +1,62 @@
 <template>
   <v-container>
-    <!-- タイトル入力 -->
-    <v-text-field
-      label="Title"
-      required
-      counter
-      maxlength="25"
-    />
+    <v-row>
+      <v-col>
+        <!-- タイトル入力 -->
+        <v-text-field
+          label="Title"
+          required
+          counter
+          maxlength="25"
+        />
+        <!-- 詳細入力 -->
+        <v-textarea
+          label="Description"
+          counter
+          maxlength="200"
+        />
+      </v-col>
+      <v-col>
+        <v-row>
+          <v-col>
+            <!-- 時刻選択 -->
+            <v-time-picker format="24hr" />
+          </v-col>
+          <v-col>
+            <!-- 日付選択 -->
+            <v-date-picker
+              no-title
+              bottom
+              color="primary"
+            />
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col>
+        <!-- 日時選択解除 -->
+        <v-list
+          class="overflow-y-auto"
+          max-height="75vh"
+        >
+          <v-list-item align="start">
+            <v-list-item-content>
+              <v-list-item-title>
+                2021/01/01
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                19:00 ~ 21:00
+              </v-list-item-subtitle>
+            </v-list-item-content>
 
-    <!-- 詳細入力 -->
-    <v-textarea
-      label="Description"
-      counter
-      maxlength="200"
-    />
-
-    <!-- 時刻選択 -->
-    <v-time-picker format="24hr" />
-
-    <!-- 日付選択 -->
-    <v-date-picker
-      no-title
-      bottom
-      color="primary"
-    />
-
-    <!-- 日時選択解除 -->
-    <v-list
-      class="overflow-y-auto"
-      max-height="75vh"
-    >
-      <v-list-item align="start">
-        <v-list-item-content>
-          <v-list-item-title>
-            2021/01/01
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            19:00 ~ 21:00
-          </v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-icon color="accent">
-            mdi-delete
-          </v-icon>
-        </v-list-item-action>
-      </v-list-item>
-    </v-list>
+            <v-list-item-action>
+              <v-icon color="accent">
+                mdi-delete
+              </v-icon>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
