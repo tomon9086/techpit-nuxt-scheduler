2a2b27f3b11f152d8aaf5367b5da10cd1c65d23f
tomon9086 <tomon9086@gmail.com>
2021-11-27 18:31:51 +0900

    add event editor component

---
+++ app/components/EventEditor.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <!-- タイトル入力 -->
+    <v-text-field
+      label="Title"
+      required
+      counter
+      maxlength="25"
+    />
+
+    <!-- 詳細入力 -->
+    <v-textarea
+      label="Description"
+      counter
+      maxlength="200"
+    />
+
+    <!-- 時刻選択 -->
+    <v-time-picker format="24hr" />
+
+    <!-- 日付選択 -->
+    <v-date-picker
+      no-title
+      bottom
+      color="primary"
+    />
+
+    <!-- 日時選択解除 -->
+    <v-list
+      class="overflow-y-auto"
+      max-height="75vh"
+    >
+      <v-list-item align="start">
+        <v-list-item-content>
+          <v-list-item-title>
+            2021/01/01
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            19:00 ~ 21:00
+          </v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-icon color="accent">
+            mdi-delete
+          </v-icon>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
