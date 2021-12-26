32b589693ddf24a39c6de6c6dcd41f7c8e644a0f
tomon9086 <tomon9086@gmail.com>
2021-12-26 12:09:08 +0900

    add event state

--- app/store/index.js
+++ app/store/index.js
@@ -1,16 +1,51 @@
 export const state = () => ({
-  isLoading: false
+  isLoading: false,
+  eventId: '',
+  title: '',
+  description: '',
+  dates: [],
+  votes: []
 })
 
 export const getters = {
   isLoading (state) {
     return state.isLoading
+  },
+  eventId (state) {
+    return state.eventId
+  },
+  title (state) {
+    return state.title
+  },
+  description (state) {
+    return state.description
+  },
+  dates (state) {
+    return state.dates
+  },
+  votes (state) {
+    return state.votes
   }
 }
 
 export const mutations = {
   setLoadingState (state, isLoading) {
     state.isLoading = !!isLoading
+  },
+  setEventId (state, eventId) {
+    state.eventId = eventId
+  },
+  setTitle (state, title) {
+    state.title = title
+  },
+  setDescription (state, description) {
+    state.description = description
+  },
+  setDates (state, dates) {
+    state.dates = dates
+  },
+  setVotes (state, votes) {
+    state.votes = votes
   }
 }
 
@@ -20,5 +55,19 @@ export const actions = {
   },
   finishLoading (ctx) {
     ctx.commit('setLoadingState', false)
+  },
+  clearEvent (ctx) {
+    ctx.commit('setEventId', '')
+    ctx.commit('setTitle', '')
+    ctx.commit('setDescription', '')
+    ctx.commit('setDates', [])
+    ctx.commit('setVotes', [])
+  },
+  setEvent (ctx, { id, title, description, dates, votes }) {
+    ctx.commit('setEventId', id)
+    title && ctx.commit('setTitle', title)
+    description && ctx.commit('setDescription', description)
+    dates && ctx.commit('setDates', dates)
+    votes && ctx.commit('setVotes', votes)
   }
 }
